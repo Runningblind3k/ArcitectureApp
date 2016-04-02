f67b247eeb65e8bf97809d37c56097638fafd0db
David <RunningBlind3k@gmail.com>
2016-04-02 17:24:13 -0400

    Added the file RegisterMemoryAssemblyCodeExample.asm as an assembly code example that can be run on the Register Memory processor. This is the code for a sorting program.

---
+++ ArcitectureApp/RegisterMemoryAssemblyCodeExample.asm
@@ -0,0 +1,40 @@
+﻿load 1 15000
+push 1
+addi 1 12000
+push 1
+addi 1 500
+push 1
+addi 1 500
+push 1
+addi 1 500
+push 1
+addi 1 500
+push 1
+pop 1
+bzero 1 156
+store 1 15020
+move 10 1
+addi 10 396
+store 10 15024
+load 5 15000
+store 1 15008
+addi 1 4
+store 1 15004
+loadi 2 15004
+loadi 3 15008
+store 3 15012
+sgt 2 15012
+bzero 2 124
+loadi 2 15004
+storei 2 15008
+storei 3 15004
+addi 5 1
+load 1 15004
+move 6 1
+sge 6 15024
+bzero 6 76
+bzero 5 48
+load 5 15000
+load 1 15020
+jump 76
+exit
